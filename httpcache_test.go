0878f91a0486fdf5591894ae40a467b26bcccaba
Michael Thorsager <thorsager@gmail.com>
2018-08-20 08:54:55 +0200

    Cleanup and Making tests work.

--- httpcache_test.go
+++ httpcache_test.go
@@ -1,11 +1,11 @@
 package httpCache
 
 import (
-	"net/http"
-	"github.com/thorsager/4chan/httpCache/memCache"
-	"testing"
 	log "github.com/sirupsen/logrus"
+	"github.com/thorsager/http-cache/memCache"
 	"io/ioutil"
+	"net/http"
+	"testing"
 )
 
 var c *http.Client
@@ -16,7 +16,7 @@ func init() {
 }
 
 func setupFunc() func() {
-	cache = &memCache.MemCache{MaxItemSize:50*1025}
+	cache = &memCache.MemCache{MaxItemSize: 50 * 1025}
 	cache.Init()
 	c = NewTransport(cache).Client()
 	return func() {
@@ -29,39 +29,36 @@ func Test_SimpleGET(t *testing.T) {
 	defer tearDown()
 
 	const url = "http://myip.dk/img/logo3.png"
-	req, err := http.NewRequest("GET",url,nil)
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		t.Errorf("Fail to create GET Request on %s : %+v",url, err)
+		t.Errorf("Fail to create GET Request on %s : %+v", url, err)
 	}
 	resp, err := c.Do(req)
-	defer resp.Body.Close()
 	if err != nil {
-		t.Errorf("Fail to Do GET-request on %s : %+v",url,err)
+		t.Errorf("Fail to Do GET-request on %s : %+v", url, err)
 	}
+	defer resp.Body.Close()
 
 	if val := resp.Header.Get("X-Cached"); val != "" {
 		t.Error("First Request CANNOT be from Cache")
 	}
 
-	if _,err = ioutil.ReadAll(resp.Body); err != nil {
+	if _, err = ioutil.ReadAll(resp.Body); err != nil {
 		t.Error("Unable to reade response-body")
 	}
 
-	t.Logf("%d %s (%s)",resp.StatusCode, resp.Status,req.URL)
+	t.Logf("%d %s (%s)", resp.StatusCode, resp.Status, req.URL)
 
 	resp2, err := c.Do(req)
 	if err != nil {
-		t.Errorf("Fail to Do GET-request on %s : %+v",url,err)
+		t.Errorf("Fail to Do GET-request on %s : %+v", url, err)
 	}
 
 	if val := resp2.Header.Get("X-Cached"); val == "" {
 		t.Error("2nd Request SHOULD be from Cache")
 	}
-	if _,err = ioutil.ReadAll(resp2.Body); err != nil {
+	if _, err = ioutil.ReadAll(resp2.Body); err != nil {
 		t.Error("Unable to reade response-body")
 	}
 
-
-
 }
-
--- memCache/mencache.go
+++ memCache/mencache.go
@@ -13,20 +13,20 @@ const sigGet int = 3
 const sigDelete int = 4
 
 type msg struct {
-	sig int
+	sig     int
 	itemKey string
-	time time.Time
+	time    time.Time
 }
 
 type MemCache struct {
 	MaxItemSize int
 	MaxIdleTime int
 	initialized bool
-	mutex sync.RWMutex
-	metaMutex sync.RWMutex
-	items map[string][]byte
-	meta map[string]itemInfo
-	syncWG  sync.WaitGroup
+	mutex       sync.RWMutex
+	metaMutex   sync.RWMutex
+	items       map[string][]byte
+	meta        map[string]itemInfo
+	syncWG      sync.WaitGroup
 
 	sideKick struct {
 		msgChan chan msg
@@ -38,18 +38,18 @@ type MemCache struct {
 
 type itemInfo struct {
 	accessCount int
-	version int
-	lastAccess time.Time
-	lastUpdate time.Time
-	created time.Time
+	version     int
+	lastAccess  time.Time
+	lastUpdate  time.Time
+	created     time.Time
 }
 
-func (c *MemCache)sidekick() {
-	skLog := log.WithField("task","sideKick")
+func (c *MemCache) sidekick() {
+	skLog := log.WithField("task", "sideKick")
 	skLog.Debug("Starting..")
 	defer c.syncWG.Done()
 	for {
-		msg,more := <-c.sideKick.msgChan
+		msg, more := <-c.sideKick.msgChan
 		if !more {
 			skLog.Debug("Terminating..")
 			break
@@ -63,40 +63,40 @@ func (c *MemCache)sidekick() {
 			break
 		case sigDelete:
 			c.metaMutex.Lock()
-			delete(c.meta,msg.itemKey)
+			delete(c.meta, msg.itemKey)
 			c.metaMutex.Unlock()
-			skLog.Debugf("Deleted %s",msg.itemKey)
+			skLog.Debugf("Deleted %s", msg.itemKey)
 			break
 		case sigGet:
 			c.metaMutex.Lock()
-			itemMeta,ok :=c.meta[msg.itemKey]
+			itemMeta, ok := c.meta[msg.itemKey]
 			if ok {
 				itemMeta.accessCount += 1
 			}
 			c.metaMutex.Unlock()
 			if ok {
-				skLog.Debugf("Updated accessCount on %s",msg.itemKey)
+				skLog.Debugf("Updated accessCount on %s", msg.itemKey)
 			}
 			break
 		case sigPut:
 			c.metaMutex.Lock()
-			if itemMeta,ok :=c.meta[msg.itemKey]; ok {
+			if itemMeta, ok := c.meta[msg.itemKey]; ok {
 				itemMeta.version += 1
 				itemMeta.lastUpdate = msg.time
 			} else {
-				c.meta[msg.itemKey] = itemInfo{version:0, lastUpdate:msg.time, accessCount:0}
+				c.meta[msg.itemKey] = itemInfo{version: 0, lastUpdate: msg.time, accessCount: 0}
 			}
 			c.metaMutex.Unlock()
-			skLog.Debugf("Updated/Created %s @%d",msg.itemKey,msg.time)
+			skLog.Debugf("Updated/Created %s @%d", msg.itemKey, msg.time)
 			break
 		default:
-			skLog.Debugf("Got un-handled %d msg on %s",msg.sig,msg.itemKey)
+			skLog.Debugf("Got un-handled %d msg on %s", msg.sig, msg.itemKey)
 		}
 	}
 }
 
-func (c *MemCache)scheduleRunner() {
-	schLog := log.WithField("task","scheduler")
+func (c *MemCache) scheduleRunner() {
+	schLog := log.WithField("task", "scheduler")
 	schLog.Debug("Starting..")
 	defer c.syncWG.Done()
 
@@ -109,17 +109,16 @@ func (c *MemCache)scheduleRunner() {
 				run = false
 				break
 			}
-		case <- time.After(500 * time.Millisecond):
+		case <-time.After(500 * time.Millisecond):
 			schLog.Debug("!!Tick!!")
 		}
 
 		if !run {
-			break
 			schLog.Debug("End-loop!!")
+			break
 		}
 	}
 }
-
 
 func (c *MemCache) Size() int {
 	return len(c.items)
@@ -154,33 +153,33 @@ func (c *MemCache) Init() {
 		go c.scheduleRunner()
 
 		log.Info("Init() called")
-		c.sideKick.msgChan <- msg{sig:sigInitialize}
+		c.sideKick.msgChan <- msg{sig: sigInitialize}
 
 		if c.MaxItemSize == 0 {
-			c.MaxItemSize = 50*1024  // Defaul to 50K
+			c.MaxItemSize = 50 * 1024 // Defaul to 50K
 		}
 	}
 }
 
 func (c *MemCache) Get(key string) (data []byte, found bool) {
 	if !c.initialized {
-		return nil,false
+		return nil, false
 	}
 	c.mutex.RLock()
-	if item,ok := c.items[key]; ok {
+	if item, ok := c.items[key]; ok {
 		data = item
 		found = true
 	}
 	c.mutex.RUnlock()
-	c.sideKick.msgChan <- msg{sig:sigGet,itemKey:key}
-	return data,found
+	c.sideKick.msgChan <- msg{sig: sigGet, itemKey: key}
+	return data, found
 }
 
 func (c *MemCache) Put(key string, data []byte) (ok bool) {
 	if c.initialized && c.WillAccept(&data) {
 		c.mutex.Lock()
 		c.items[key] = data
-		c.sideKick.msgChan <- msg{sig: sigPut, itemKey: key, time:time.Now()}
+		c.sideKick.msgChan <- msg{sig: sigPut, itemKey: key, time: time.Now()}
 		c.mutex.Unlock()
 		return true
 	}
@@ -200,15 +199,13 @@ func (c *MemCache) Key(r *http.Request) string {
 	return r.URL.String()
 }
 
-func (c *MemCache) WillAccept(data *[]byte) bool{
-	log.Debugf("%d vs %d", len(*data),c.MaxItemSize)
+func (c *MemCache) WillAccept(data *[]byte) bool {
+	log.Debugf("%d vs %d", len(*data), c.MaxItemSize)
 	return len(*data) < c.MaxItemSize
 }
 
 func New(maxItemSize int) *MemCache {
-	c := MemCache{ MaxItemSize:maxItemSize }
+	c := MemCache{MaxItemSize: maxItemSize}
 	c.Init()
 	return &c
 }
-
-
